bbb2bdfcac02d94d0c27c6081912ca32e8322fb3
Ben Russell <mountainbrussells@gmail.com>
2017-04-12 14:35:36 -0600

    Add private and data contexts

--- KidTimer/BARPersistenceController.swift
+++ KidTimer/BARPersistenceController.swift
@@ -14,31 +14,52 @@ import XCTest
 class BARPersistenceController: NSObject {
     
     var closure: () = {() -> () in }()
+    
+    lazy var privateObjectContext: NSManagedObjectContext =
+        {
+            let modelURL = Bundle.main.url(forResource: "KidTimer", withExtension: "momd")
+            let mom = NSManagedObjectModel(contentsOf: modelURL!)
+            assert(mom != nil, "Error initializing mom from: \(String(describing: modelURL))")
+            
+            let psc = NSPersistentStoreCoordinator(managedObjectModel: mom!)
+            
+            let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+            let storeURL = (urls[urls.endIndex-1]).appendingPathComponent("KidTimer.sqlite")
+            
+            var store:NSPersistentStore?
+            do { store =  try psc.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: storeURL, options: nil)
+            } catch {
+                XCTFail("error creating store: \(error)")
+            }
+            
+            
+            
+            var managedObjectContext = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
+            managedObjectContext.persistentStoreCoordinator = psc
+            
+            if self.closure {
+               self.closure()
+            }
+            return managedObjectContext
+    }()
+    
     lazy var managedObjectContext: NSManagedObjectContext =
     {
-        let modelURL = Bundle.main.url(forResource: "KidTimer", withExtension: "momd")
-        let mom = NSManagedObjectModel(contentsOf: modelURL!)
-        assert(mom != nil, "Error initializing mom from: \(modelURL)")
-            
-        let psc = NSPersistentStoreCoordinator(managedObjectModel: mom!)
-            
-        let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        let storeURL = (urls[urls.endIndex-1]).appendingPathComponent("KidTimer.sqlite")
-        
-        var store:NSPersistentStore?
-        do { store =  try psc.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: storeURL, options: nil)
-        } catch {
-            XCTFail("error creating store: \(error)")
-        }
-        
-    
-            
-        var managedObjectContext = NSManagedObjectContext.init(concurrencyType: private​Queue​Concurrency​Type)
-        managedObjectContext.persistentStoreCoordinator = psc
+        var managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+        managedObjectContext.parent = self.privateObjectContext
             
         return managedObjectContext
     }()
-    var dataContext: NSManagedObjectContext
+    
+    lazy var dataContext: NSManagedObjectContext =
+        {
+            var managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+            managedObjectContext.parent = self.managedObjectContext
+            
+            return managedObjectContext
+    }()
+    
+    
     
     init (closure:() -> ())
     {
